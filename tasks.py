3648cd243f537aae04ddcf2b32010550020cece6
grevlinger <155060026+grevlinger@users.noreply.github.com>
2024-01-04 12:02:41 +0100

    Create tasks.py

---
+++ tasks.py
@@ -0,0 +1,9 @@
+tasks = [
+    {
+        'id': 1,
+        'title': 'Kryptering av kjemiske formler',
+        'description': 'Dekrypter den krypterte teksten for å finne kjemiske formler.',
+        'flag': 'CBRNE_FLAG_1',
+    },
+    # Legg til flere oppgaver her
+]
