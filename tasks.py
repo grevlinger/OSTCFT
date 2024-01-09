adf209f704717b76df000824e0569e132bbc580b
grevlinger <155060026+grevlinger@users.noreply.github.com>
2024-01-09 12:11:11 +0100

    Delete tasks.py

--- tasks.py
+++
@@ -1,9 +0,0 @@
-tasks = [
-    {
-        'id': 1,
-        'title': 'Kryptering av kjemiske formler',
-        'description': 'Dekrypter den krypterte teksten for å finne kjemiske formler.',
-        'flag': 'CBRNE_FLAG_1',
-    },
-    # Legg til flere oppgaver her
-]
